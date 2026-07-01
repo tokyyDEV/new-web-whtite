cffc02a8b3117a66648d80b23bd21f8a92be7268
tokyyDEV <whitecorest@gmail.com>
2026-07-01 23:53:25 +0200

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-white/10 py-8 text-center text-gray-500">
-      © 2026 WhiteCore. All rights reserved.
-    </footer>
-  );
-}
